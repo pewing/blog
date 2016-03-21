53ef9960282a4a4d07135c179415e83210759b2d
pewing <philip.jh.ewing@gmail.com>
2016-03-21 16:16:12 -0700

    add foundation for a navigation bar

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+
+  def index
+    @message = "This is a work in progress. Come back later!"
+    puts @message
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+
+  root to: 'pages#home'
+
+  scope controller: 'pages' do
+    get 'home'
+  end
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
