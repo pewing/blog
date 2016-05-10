8b6491adf8edd901a054aa6d20e72caadbf611ae
pewing <philip.jh.ewing@gmail.com>
2016-05-10 15:12:28 -0700

    add fading location text

---
+++ app/assets/javascripts/about.js
@@ -0,0 +1,15 @@
+$('.pages.ready').ready(function() {
+
+    var $location_hover = $('#hover-location');
+
+    var reset_location = function() {
+        $location_hover.fadeOut(600);
+    };
+
+    $('#dot-sf').hover(function() {
+        $location_hover.text('About Me').fadeIn(600);
+    }, reset_location);
+
+
+
+});
