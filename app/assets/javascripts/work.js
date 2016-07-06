3a9de0e0998cb39943799f21664f45e39f29226e
pewing <philip.jh.ewing@gmail.com>
2016-07-05 18:44:56 -0700

    skill list needs to be automatically on the right

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -1,36 +1,46 @@
 $('.pages.work').ready(function() {
     $skills_box = $('#skills-box');
     $work_experience = $('#work-experience');
-    $work_experience_top = $work_experience.position().top;
+    $work_experience_top = $work_experience.offset().top;
     $work_freelance = $('#work-freelance');
-    $work_freelance_top = $work_freelance.position().top;
+    $work_freelance_top = $work_freelance.offset().top;
     $work_hifi = $('#work-hifi');
-    $work_hifi_top = $work_hifi.position().top;
+    $work_hifi_top = $work_hifi.offset().top;
     $skills_list = $('#skills-list');
     $all_skills = $('.show-all');
 
     //$skills_box.fadeOut();
 
+    $( window ).click(function(e) {
+        console.log(e.pageY);
+        console.log($(this).height());
+    });
+
+
     $skills_box.affix({
-        offset: { top: 0 }
+        //offset: { top: $work_experience_top - $( window ).height()}
+        offset: { top: ($( window ).height())/2 }
     });
 
     function specify_skills(job) {
         $all_skills.each(function() {
             if (!$( this).hasClass(job)) {
-                $(this).fadeTo("slow", 0);
+                $(this).fadeTo("slow", 0.3);
+            }
+            else {
+                $(this).fadeTo("slow", 1);
             }
         });
     }
 
     $( window ).scroll(function() {
         //$skills_box.removeClass('hidden');
-        var scroll = $( this ).scrollTop();
+        var scroll = $( this ).scrollTop() + ($( this).height())/2;
 
-        console.log(scroll, $work_experience_top);
+        //console.log(scroll, $work_freelance_top, $work_experience_top, $( this).height());
 
         if (scroll > $work_freelance_top) {
-            $skills_box.fadeIn("slow");
+            //$skills_box.fadeIn("slow");
             if (scroll > $work_freelance_top && scroll < $work_hifi_top) {
                 specify_skills('show-freelance');
             }
@@ -55,75 +65,3 @@ $('.pages.work').ready(function() {
 
 
 });
-
-
-//google.load('visualization', '1.0', {'packages':['corechart'], 'callback': drawChart});
-//
-//function drawChart() {
-//    var data = google.visualization.arrayToDataTable([
-//        ['Date', '', 'Expenses'],
-//        ['2004',  1000,      400],
-//        ['2005',  1170,      460],
-//        ['2006',  660,       1120],
-//        ['2007',  1030,      540]
-//    ]);
-//
-//    var options = {
-//        //curveType: 'function',
-//        legend: { position: 'bottom' },
-//        //backgroundColor: { fill: 'transparent'}
-//    };
-//
-//    var chart = new google.visualization.LineChart(document.getElementById('work-timeline-container'));
-//
-//    chart.draw(data, options);
-//}
-
-//var data = new google.visualization.DataTable(allowHtml=true);
-//data.addColumn('string', 'Role');
-//data.addColumn('string', 'Job');
-//data.addColumn('date', 'Start');
-//data.addColumn('date', 'End');
-//data.addRows([
-//    ['School', 'Hamilton College', new Date(2011,9), new Date(2015, 5)],
-//    ['Work', 'Freelancer', new Date(2016,3), new Date()],
-//    ['Work', 'High Fidelity', new Date(2015,10), new Date(2016,3)],
-//    ['Work', 'Software Engineer', new Date(2015,1), new Date(2015,5)],
-//    ['Work', 'CS Research Assistant', new Date(2014,5), new Date(2014,7)]
-//]);
-////data.addRows([
-////    ['Hamilton College', new Date(2011,9), new Date(2015, 5)],
-////    [Freelancer', new Date(2016,3), new Date()],
-////    ['High Fidelity', new Date(2015,10), new Date(2016,3)],
-////    ['Software Engineer', new Date(2015,1), new Date(2015,5)],
-////    ['CS Research Assistant', new Date(2014,5), new Date(2014,7)]
-////]);
-////data.setProperty(0, 0,'style', 'border: 0');
-//
-//var options = {
-//    //'width':600,
-//    height: 150,
-//    backgroundColor: '#171F24',
-//    //strokeWidth: 0,
-//    timeline: {
-//        //barLabelStyle: { fontName: 'Droid Sans', fontSize: 10, color: '#DFE5F7' },
-//        groupByRowLabel: true,
-//        showRowLabels: false,
-//        showBarLabels: false
-//    },
-//    forceIFrame: true,
-//    avoidOverlappingGridLines: false
-//    //backgroundColor: {
-//    //stroke: '#4322c0',
-//    //strokeWidth: 0
-//    //}
-//    //vAxis: {
-//    //    textStyle: { color: 'white' }
-//    //},
-//    //hAxis: {
-//    //    textStyle: { color: '#DFE5F7'}
-//    //}
-//    //'is3D':true
-//};
-//var chart = new google.visualization.Timeline(document.getElementById('work-timeline-container'));
-//chart.draw(data, options);
