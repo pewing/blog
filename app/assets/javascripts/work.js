be2e071bd6ebd59290a17d1c8e8f6cb24baf1f66
pewing <philip.jh.ewing@gmail.com>
2016-07-05 21:20:12 -0700

    betetr variable names

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -1,32 +1,30 @@
 $('.pages.work').ready(function() {
 
-    console.log("AAAAH");
-
-    var skills_box = $('#skills-box');
+    var $skills_box = $('#skills-box');
     //var work_experience = $('#work-experience');
     //var $work_experience_top = $work_experience.offset().top;
-    var work_freelance = $('#work-freelance');
-    var work_freelance_top = work_freelance.offset().top;
-    var work_hifi = $('#work-hifi');
-    var work_hifi_top = work_hifi.offset().top;
+    var $work_freelance = $('#work-freelance');
+    var work_freelance_top = $work_freelance.offset().top;
+    var $work_hifi= $('#work-hifi');
+    var work_hifi_top = $work_hifi.offset().top;
     //var $skills_list = $('#skills-list');
-    var all_skills = $('.show-all');
+    var $all_skills = $('.show-all');
 
 
-    skills_box.affix({
-        offset: { top: (skills_box.position().top - ($( window ).height())/2) }
+    $skills_box.affix({
+        offset: { top: ($skills_box.position().top - ($( window ).height())*0.4) }
     });
 
-    skills_box.on('affixed.bs.affix', function () {
-        $(this).css('top', ($( window ).height())/2);
+    $skills_box.on('affixed.bs.affix', function () {
+        $(this).css('top', '40%');
     });
 
-    skills_box.on('affixed-top.bs.affix', function () {
+    $skills_box.on('affixed-top.bs.affix', function () {
         $(this).removeAttr('style');
     });
 
     function specify_skills(job) {
-        all_skills.each(function() {
+        $all_skills.each(function() {
             if (!$( this).hasClass(job)) {
                 $( this ).fadeTo("slow", 0.3);
             }
@@ -49,10 +47,11 @@ $('.pages.work').ready(function() {
             }
         }
         //else {
-            //$skills_box.fadeOut("slow", function() {
+            //$$skills_box.fadeOut("slow", function() {
             //    //console.log($work_freelance_top);
             //    //console.log(scroll);
             //});
         //}
     });
 });
+//TODO: affix position not reset when page is refreshed
