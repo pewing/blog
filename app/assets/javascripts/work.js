d21989bea8cf6d9bdb44e7b2a02a8d23e4bceecf
pewing <philip.jh.ewing@gmail.com>
2016-07-06 16:37:59 -0700

    switch skill fading to scrollspy

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -10,6 +10,9 @@ $('.pages.work').ready(function() {
     var $work_hifi= $('#work-hifi');
     var work_hifi_top = $work_hifi.offset().top;
     var work_hifi_bottom = work_hifi_top + $work_hifi.height();
+    var $work_hamilton= $('#work-hamilton');
+    var work_hamilton_top = $work_hamilton.offset().top;
+    var work_hamilton_bottom = work_hamilton_top + $work_hamilton.height();
     //var $skills_list = $('#skills-list');
     var $all_skills = $('.show-all');
     var $window = $( window );
@@ -23,7 +26,7 @@ $('.pages.work').ready(function() {
     $skills_box.affix({
         offset: {
             top: (skills_box_top - (window_height)*0.4),
-            bottom: $( document).height() - work_hifi_bottom
+            bottom: $( document).height() - work_hamilton_bottom
         }
     });
 
@@ -40,53 +43,71 @@ $('.pages.work').ready(function() {
         $skills_box.removeClass('put-middle');
     });
 
-    function specify_skills(job) {
-        //console.log("in specify skills");
+    $('body').scrollspy({
+        target: '#skills-box',
+        offset: window_height*0.4
+    });
+
+    $('body').on('activate.bs.scrollspy', function() {
         $all_skills.each(function() {
-            var $skill = $( this );
-            if (!$skill.hasClass(job)) {
-                //console.log("1");
+           var $skill = $( this );
+            if ($skill.hasClass('active')) {
+                $skill.fadeTo("slow", 1);
+            }
+            else {
                 $skill.fadeTo("slow", 0.3);
             }
-            else {
-                $skill.fadeTo("slow", 1);
-                //console.log("2");
-            }
         });
-    }
-
-    function test_scroll(scroll) {
-        //console.log(scroll);
-        if (scroll > work_freelance_top) {
-            if (scroll > work_freelance_top && scroll < work_freelance_bottom) {
-                //console.log("freelancing");
-                specify_skills('show-freelance');
-            }
-
-            else if (scroll > work_hifi_top && scroll < work_hifi_bottom) {
-                //console.log("hifi");
-                specify_skills('show-hifi');
-            }
-
-            else {
-                specify_skills('show-all');
-            }
-        }
-        else {
-            specify_skills('show-all');
-        }
-    }
-
-    //$window.scroll(function() {
-    //   test_scroll($window.scrollTop() + (window_height)/2);
-    //});
+    });
 
 
-    //    //var $win = $( this );
-    //    var scroll = $window.scrollTop() + (window_height)/2;
-    //
-    //
-    //});
 });
 
-//TODO: scrolling color change is hella slow
+
+//function specify_skills(job) {
+//    //console.log("in specify skills");
+//    $all_skills.each(function() {
+//        var $skill = $( this );
+//        if (!$skill.hasClass(job)) {
+//            //console.log("1");
+//            $skill.fadeTo("slow", 0.3);
+//        }
+//        else {
+//            $skill.fadeTo("slow", 1);
+//            //console.log("2");
+//        }
+//    });
+//}
+//
+//function test_scroll(scroll) {
+//    //console.log(scroll);
+//    if (scroll > work_freelance_top) {
+//        if (scroll > work_freelance_top && scroll < work_freelance_bottom) {
+//            //console.log("freelancing");
+//            specify_skills('show-freelance');
+//        }
+//
+//        else if (scroll > work_hifi_top && scroll < work_hifi_bottom) {
+//            //console.log("hifi");
+//            specify_skills('show-hifi');
+//        }
+//
+//        else {
+//            specify_skills('show-all');
+//        }
+//    }
+//    else {
+//        specify_skills('show-all');
+//    }
+//}
+
+//$window.scroll(function() {
+//   test_scroll($window.scrollTop() + (window_height)/2);
+//});
+
+
+//    //var $win = $( this );
+//    var scroll = $window.scrollTop() + (window_height)/2;
+//
+//
+//});
