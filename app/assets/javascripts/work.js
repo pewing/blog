f5db55d883f2807a22638676b7edecc4072f2c2c
pewing <philip.jh.ewing@gmail.com>
2016-07-05 20:52:46 -0700

    standardize format

--- app/assets/javascripts/work.js
+++ app/assets/javascripts/work.js
@@ -1,57 +1,50 @@
 $('.pages.work').ready(function() {
-    $skills_box = $('#skills-box');
-    $work_experience = $('#work-experience');
-    $work_experience_top = $work_experience.offset().top;
-    $work_freelance = $('#work-freelance');
-    $work_freelance_top = $work_freelance.offset().top;
-    $work_hifi = $('#work-hifi');
-    $work_hifi_top = $work_hifi.offset().top;
-    $skills_list = $('#skills-list');
-    $all_skills = $('.show-all');
 
-    //$skills_box.fadeOut();
+    console.log("AAAAH");
 
-    $( window ).click(function(e) {
-        console.log(e.pageY);
-        console.log($(this).height());
+    var skills_box = $('#skills-box');
+    //var work_experience = $('#work-experience');
+    //var $work_experience_top = $work_experience.offset().top;
+    var work_freelance = $('#work-freelance');
+    var work_freelance_top = work_freelance.offset().top;
+    var work_hifi = $('#work-hifi');
+    var work_hifi_top = work_hifi.offset().top;
+    //var $skills_list = $('#skills-list');
+    var all_skills = $('.show-all');
+
+
+    skills_box.affix({
+        offset: { top: (skills_box.position().top - ($( window ).height())/2) }
     });
 
-
-    $skills_box.affix({
-        offset: { top: ($skills_box.position().top - ($( window ).height())/2) }
-    });
-
-    $skills_box.on('affixed.bs.affix', function () {
-        console.log($( window ).height()/2);
+    skills_box.on('affixed.bs.affix', function () {
         $(this).css('top', ($( window ).height())/2);
-        //$(this).css('top', 0);
     });
 
-    $skills_box.on('affixed-top.bs.affix', function () {
+    skills_box.on('affixed-top.bs.affix', function () {
         $(this).removeAttr('style');
     });
 
     function specify_skills(job) {
-        $all_skills.each(function() {
+        all_skills.each(function() {
             if (!$( this).hasClass(job)) {
-                $(this).fadeTo("slow", 0.3);
+                $( this ).fadeTo("slow", 0.3);
             }
             else {
-                $(this).fadeTo("slow", 1);
+                $( this ).fadeTo("slow", 1);
             }
         });
     }
 
     $( window ).scroll(function() {
-        var scroll = $( this ).scrollTop() + ($( this).height())/2;
+        var scroll = $( window ).scrollTop() + ($( this ).height())/2;
 
-        if (scroll > $work_freelance_top) {
-            //$skills_box.fadeIn("slow");
-            if (scroll > $work_freelance_top && scroll < $work_hifi_top) {
+        if (scroll > work_freelance_top) {
+            if (scroll > work_freelance_top && scroll < work_hifi_top) {
                 specify_skills('show-freelance');
             }
 
-            if (scroll > $work_hifi_top && scroll < $work_hifi_top) {
+            if (scroll > work_hifi_top && scroll < work_hifi_top) {
                 specify_skills('show-hifi');
             }
         }
