d1e81dd19b433eaeb79c0bd3e75859dae3c0c900
pewing <philip.jh.ewing@gmail.com>
2016-07-14 14:46:36 -0700

    creation of recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,30 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
+  end
+
+  def edit
+    @recipe = Recipe.find_by_name(params[:name])
+  end
+
+  def create
+    puts "-----"
+    puts params[:recipe]
+    @recipe = Recipe.new(
+        name: params[:recipe][:name],
+        category: params[:recipe][:category],
+        ingredients: params[:recipe][:ingredients],
+        instructions: params[:recipe][:instructions]
+    )
+    begin
+      if @recipe.save
+        flash[:success] = "Recipe succesfully saved"
+        render :index and return
+      end
+    rescue StandardError => e
+      logger.error "Error in recipe creation: #{ e.message }"
+    end
 
   end
 
