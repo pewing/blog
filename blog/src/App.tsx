22612e17d772b1d580c3a94bf521d4185149291a
Philip Ewing <philipewing@lucass-MacBook-Pro.local>
2022-08-15 18:27:08 -0700

    initial react app. basic home page with Coming Soon. delete old app

---
+++ blog/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./components/Home";
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
