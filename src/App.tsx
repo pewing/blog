0f7fab36cc416fde1313cc273df6a8fb5a0d8f16
Philip Ewing <philipewing@lucass-MacBook-Pro.local>
2022-08-24 15:11:58 -0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 
+import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
+import Layout from "./components/Layout";
+import Work from "./components/Work";
+
 import './App.scss';
 
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+        <Layout>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/work" element={<Work />} />
+              <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Layout>
     </div>
   );
 }
---
+++ src/components/About/About.tsx
@@ -0,0 +1,13 @@
+import logo from 'Images/logo_hole.png';
+
+import './About.scss';
+
+const About = () => {
+    return (
+      <div className="about">
+        coming soon!
+      </div>
+    );
+  };
+  
+export default About;
