a9211be66e7b1f8ee97ae0fbc3b7eea9646f4b69
Philip Ewing <philipewing@lucass-MacBook-Pro.local>
2022-08-24 16:47:24 -0700

    add Contact page

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -1,11 +1,50 @@
-import logo from 'Images/logo_hole.png';
+import { Link } from "react-router-dom";
+
+import angelList from 'Images/angel_logo.png';
+import github from 'Images/github_logo.png';
+import linkedin from 'Images/linkedin_logo.png';
 
 import './Contact.scss';
 
 const Contact = () => {
     return (
       <div className="contact">
-        coming soon!
+        <div className="reach-me">
+          Do you have any questions or comments? Email me at
+          <div className="reach-me-email">
+            <Link 
+              to='javascript:void(0)'
+              onClick={() => window.location.href = 'mailto:philip.sp.ewing@gmail.com'}
+            >
+              philip.sp.ewing@gmail.com
+            </Link>
+          </div>
+        </div>
+        <div className="reach-me-websites">
+          Or you can reach me on one of these places:
+          <div className="reach-me-websites-list">
+            <ul>
+              <li>
+                <a href="https://github.com/pewing">
+                  <img src={github} alt={"PE"} />
+                  &nbsp;pewing
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/philipewing">
+                  <img src={linkedin} alt={"PE"} />
+                  &nbsp;philipewing
+                </a>
+              </li>
+              <li>
+                <a href="https://angel.co/philip-ewing-1">
+                  <img src={angelList} alt={"PE"} />
+                  &nbsp;philip-ewing-1
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   };
